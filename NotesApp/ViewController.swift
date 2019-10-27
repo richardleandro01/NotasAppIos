fda187264ba33c7bdb6f79257cd1f0024b6e5ae8
richard <richardp.leandro@gmail.com>
2019-10-27 15:37:55 -0300

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by richardleandro on 27/10/19.
+//  Copyright © 2019 richardleandro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
